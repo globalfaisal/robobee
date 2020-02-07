ec8e43debd80c6698f99fd7ba7190afc352bd638
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-02-07 11:15:16 +0100

    Fixing a style bug in mobile view

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -6,7 +6,9 @@ import './hero.css';
 
 const Hero = () => (
   <div className="hero">
-    <h2 className="heading">Robots there’s more to them than meets the eye.</h2>
+    <h3 className="heading">
+      Robots, there’s more to them than meets the eye.
+    </h3>
     <SectionLink targetSectionId="section-demo" />
   </div>
 );
