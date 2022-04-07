3f8cd0d4b1d6bf0e8d4c31a8dced2f8eb2bb9948
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2022-04-07 18:34:02 +0200

    Refactor: use react hooks instead

--- src/components/demo/demo.jsx
+++ src/components/demo/demo.jsx
@@ -1,65 +1,50 @@
-import React, { Component } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Robohash from 'react-robohash';
-
 import ImageCard from '../image-card/image-card';
 import SearchBox from '../search-box/search-box';
-
 import './demo.css';
 
-class Demo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robohash: '',
-      searchValue: '',
-      loading: false,
+export default function Demo() {
+  let timeout;
+  const [robohash, setRobohash] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    setRobohash('default');
+    setLoading(false);
+
+    return () => {
+      window.clearTimeout(timeout);
     };
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  componentDidMount() {
-    this.setState({
-      robohash: 'deafult',
-      loading: false,
-    });
-  }
-
-  componentWillUnmount() {
-    window.clearTimeout(this.timeout);
-  }
-
-  handleSearchSubmit = e => {
+  const handleSearchSubmit = e => {
     e.preventDefault();
-    const { searchValue } = this.state;
-    this.setState({ loading: true });
-    this.timeout = window.setTimeout(() => {
-      this.setState({
-        robohash: searchValue,
-        loading: false,
-      });
+    setLoading(true);
+    timeout = window.setTimeout(() => {
+      setLoading(false);
+      setRobohash(searchValue);
     }, 2000);
   };
 
-  render() {
-    const { searchValue, robohash, loading } = this.state;
-    return (
-      <section className="section-demo" id="section-demo">
-        <div className="content">
-          <h3 className="title">GENERATE UNIQUE ROBOTS FROM ANY TEXT</h3>
-          <SearchBox
-            type="search"
-            value={searchValue}
-            onSearchChange={e => this.setState({ searchValue: e.target.value })}
-            onSearchSubmit={this.handleSearchSubmit}
-            placeholder="Write anything..."
-            loading={loading}
-          />
-          <Robohash name={robohash} size={300}>
-            {imgUrl => <ImageCard src={imgUrl} alt={`Robobee-${robohash}`} />}
-          </Robohash>
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section className="section-demo" id="section-demo">
+      <div className="content">
+        <h3 className="title">GENERATE UNIQUE ROBOTS FROM ANY TEXT</h3>
+        <SearchBox
+          type="search"
+          value={searchValue}
+          onSearchChange={e => setSearchValue(e.target.value)}
+          onSearchSubmit={handleSearchSubmit}
+          placeholder="Write anything..."
+          loading={loading}
+        />
+        <Robohash name={robohash} size={300}>
+          {imgUrl => <ImageCard src={imgUrl} alt={`Robobee-${robohash}`} />}
+        </Robohash>
+      </div>
+    </section>
+  );
 }
-export default Demo;
